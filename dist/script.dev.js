03c6fe458a80201b6279b6a46bddc45b8f1dc4ac
fake-programmer <ndegeezra05@gmail.com>
2025-02-27 18:32:41 +0300

    my first commit

---
+++ dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  Document.getElementById('enableScrollBar').addEventListener('click', function () {
+    document.body.style.overflow = 'auto';
+  });
+});
+//# sourceMappingURL=script.dev.js.map
